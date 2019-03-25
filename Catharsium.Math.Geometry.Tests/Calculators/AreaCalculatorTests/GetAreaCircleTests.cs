b0bcbad1e0a8d2da56aaa8f6f193953ba2c0718e
Thierry Brachthuizer <thierry.brachthuizer@unifiedpost.com>
2019-03-25 12:46:31 +0100

    #1 AreaCalculator.GetArea(Circle)

--- Catharsium.Math.Geometry.Tests/Calculators/AreaCalculatorTests/GetAreaCircleTests.cs
+++ Catharsium.Math.Geometry.Tests/Calculators/AreaCalculatorTests/GetAreaCircleTests.cs
@@ -1,4 +1,5 @@
 ﻿using Catharsium.Math.Geometry.Calculators;
+using Catharsium.Math.Geometry.Models;
 using Catharsium.Util.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,9 +9,41 @@ namespace Catharsium.Math.Geometry.Tests.Calculators.AreaCalculatorTests
     public class GetAreaCircleTests : TestFixture<AreaCalculator>
     {
         [TestMethod]
-        public void GetArea_()
+        public void GetArea_NoRadius_Returns0()
         {
+            var circle = new Circle(0, 0, 0);
+            var actual = this.Target.GetArea(circle);
+            Assert.AreEqual(0, actual);
+        }
 
+
+        [TestMethod]
+        public void GetArea_LocationIsIrrelevant()
+        {
+            var reference = new Circle(0, 0, 2);
+            var circle = new Circle(1, 2, reference.Radius);
+
+            var expected = this.Target.GetArea(reference);
+            var actual = this.Target.GetArea(circle);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void GetArea_Radius1_ReturnsPi()
+        {
+            var circle = new Circle(0, 0, 1);
+            var actual = this.Target.GetArea(circle);
+            Assert.AreEqual(System.Math.PI, actual);
+        }
+        
+
+        [TestMethod]
+        public void GetArea_RadiusX_ReturnsRadiusSquaredTimesPi()
+        {
+            var circle = new Circle(0, 0, 10);
+            var actual = this.Target.GetArea(circle);
+            Assert.AreEqual(System.Math.PI * circle.Radius * circle.Radius, actual);
         }
     }
 }
--- Catharsium.Math.Geometry/Calculators/AreaCalculator.cs
+++ Catharsium.Math.Geometry/Calculators/AreaCalculator.cs
@@ -6,15 +6,29 @@ namespace Catharsium.Math.Geometry.Calculators
 {
     public class AreaCalculator : IAreaCalculator
     {
+        private readonly ICircumferenceCalculator circumferenceCalculator;
+        private readonly IDistanceCalculator distanceCalculator;
+
+        public AreaCalculator(ICircumferenceCalculator circumferenceCalculator, IDistanceCalculator distanceCalculator)
+        {
+            this.circumferenceCalculator = circumferenceCalculator;
+            this.distanceCalculator = distanceCalculator;
+        }
+
+
         public double GetArea(Circle c)
         {
-            throw new NotImplementedException();
+            return System.Math.PI * System.Math.Pow(c.Radius, 2);
         }
 
 
         public double GetArea(Triangle t)
         {
             throw new NotImplementedException();
+            // O^2 = s * (s - a) * (s - b) * (s - c)
+            var s = this.circumferenceCalculator.GetCircumference(t) / 2;
+            return System.Math.Sqrt(s * (s - this.distanceCalculator.GetLength(t.GetLineA())) *
+                                    (s - this.distanceCalculator.GetLength(t.GetLineB())) * (s - this.distanceCalculator.GetLength(t.GetLineC())));
         }
     }
 }
